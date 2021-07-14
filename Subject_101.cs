ca9a096b90fb0e0f951a4dd24060583d22a55f47
HailongWu <13126506430@163.com>
2021-07-14 09:47:24 +0800

    101. 对称二叉树

---
+++ Subject_101.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Hot100
+{
+    /// <summary>
+    /// 101. 对称二叉树
+    /// https://leetcode-cn.com/problems/symmetric-tree/
+    /// </summary>
+    class Subject_101 : ISubject
+    {
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSymmetric(TreeNode root)
+        {
+            return check(root, root);
+        }
+
+        public bool check(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            if (p == null || q == null)
+            {
+                return false;
+            }
+            return p.val == q.val && check(p.left, q.right) && check(p.right, q.left);
+        }
+
+  
+    }
+}
